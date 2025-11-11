3c4a76fa25b5e00f22139ab191f850ed18a71bbe
André <andre@iburger.com.br>
2025-11-11 19:40:25 -0300

    feat: Atualiza Exemplos Unidade 07

--- PraticaPOO/src/main/unidade07/exemplos/exemplo02/view/AplicacaoCadastroEstudantes.java
+++ PraticaPOO/src/main/unidade07/exemplos/exemplo02/view/AplicacaoCadastroEstudantes.java
@@ -12,10 +12,8 @@ import javax.swing.SwingUtilities;
 public class AplicacaoCadastroEstudantes {
 
 	public static void main(String[] args) {
-		// Inicia a aplicação na thread de eventos do Swing
-		SwingUtilities.invokeLater(() -> {
-			JanelaCadastroEstudantes janela = new JanelaCadastroEstudantes();
-			janela.setVisible(true);
-		});
+		// Inicia a aplicação na do Swing
+		JanelaCadastroEstudantes janela = new JanelaCadastroEstudantes();
+		janela.setVisible(true);
 	}
 }
